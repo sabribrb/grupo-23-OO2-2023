07ecc89dd355d91bca0f29d53a023432842d958a
sabrina <sabribarbiero@hotmail.com>
2023-05-28 20:42:58 -0300

    primer commit a mi branch

---
+++ src/main/java/com/unla/grupo23/Grupo23Application.java
@@ -0,0 +1,13 @@
+package com.unla.grupo23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo23Application.class, args);
+	}
+
+}
