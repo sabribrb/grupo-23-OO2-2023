77061212a6ff03d4930f8fc57c628b4371c1501e
sabrina <sabribarbiero@hotmail.com>
2023-05-28 20:42:58 -0300

    primer commit a mi branch

---
+++ src/test/java/com/unla/grupo23/Grupo23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
