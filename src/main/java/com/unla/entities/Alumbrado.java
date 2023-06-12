d8f9babf13368483f961ababebeccc4d140c8b9d
caro <caroaguirre999@gmail.com>
2023-06-12 19:01:58 -0300

    arreglo constructor de alumbrado

--- src/main/java/com/unla/entities/Alumbrado.java
+++ src/main/java/com/unla/entities/Alumbrado.java
@@ -19,8 +19,11 @@ public class Alumbrado extends Dispositivo{
 	
 	public Alumbrado() {}
 
-	public Alumbrado(String nombre, LocalDateTime creacion, boolean activo, Set<Evento> eventos, int numAula) {
-		super(nombre, creacion, activo, eventos);
+	public Alumbrado(String nombre, LocalDateTime creacion, boolean activo, Edificio edificio, Set<Evento> eventos,
+			int numAula) {
+		super(nombre, creacion, activo, edificio, eventos);
 		this.numAula = numAula;
 	}
+
+
 }
